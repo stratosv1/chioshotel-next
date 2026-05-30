a5f55f6742d8fa0fd55c9fd0289cac112fb6e780
stratosv1 <44983464+stratosv1@users.noreply.github.com>
2026-05-30 10:27:30 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "chioshotel.gr",
-        pathname: "/wp-content/uploads/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.trustindex.io",
-        pathname: "/**",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
