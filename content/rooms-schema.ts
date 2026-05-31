ed687e277a8ca1d8eb16a05f31413b0da4090c46
stratosv1 <evoulamandis@gmail.com>
2026-05-31 11:35:17 +0300

    Add rooms category page template

---
+++ content/rooms-schema.ts
@@ -0,0 +1,34 @@
+export const roomsCategorySchema = {
+  "@context": "https://schema.org",
+  "@type": "ItemList",
+  name: "Chios Rooms & Apartments - Voulamandis House",
+  description:
+    "Explore accommodation categories in Chios at Voulamandis House, including economy double rooms, ground floor rooms, first floor rooms and family apartments.",
+  url: "https://chioshotel.gr/chios-rooms/",
+  itemListElement: [
+    {
+      "@type": "ListItem",
+      position: 1,
+      url: "https://chioshotel.gr/chios-rooms/economy-double-rooms/",
+      name: "Economy Double Room",
+    },
+    {
+      "@type": "ListItem",
+      position: 2,
+      url: "https://chioshotel.gr/chios-rooms/standard-double-room/",
+      name: "Ground Floor Rooms - Double & Triple",
+    },
+    {
+      "@type": "ListItem",
+      position: 3,
+      url: "https://chioshotel.gr/chios-rooms/standard-double-room/",
+      name: "First Floor Rooms - Double & Triple",
+    },
+    {
+      "@type": "ListItem",
+      position: 4,
+      url: "https://chioshotel.gr/chios-rooms/family-chios-apartments/",
+      name: "Family Apartments",
+    },
+  ],
+};
