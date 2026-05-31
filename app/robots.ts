113763ea38b8e69a696d8bec4b6532327a4fce0c
stratosv1 <evoulamandis@gmail.com>
2026-05-31 12:52:59 +0300

    Allow Next.js assets in robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,23 +1,13 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://chioshotel.gr";
-
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: [
-          "/api/",
-          "/_next/",
-          "/private/",
-          "/admin/",
-          "/wp-admin/",
-        ],
-      },
-    ],
-    sitemap: `${siteUrl}/sitemap.xml`,
-    host: siteUrl,
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/", "/private/", "/admin/", "/wp-admin/"],
+    },
+    host: "https://chioshotel.gr",
+    sitemap: "https://chioshotel.gr/sitemap.xml",
   };
 }
