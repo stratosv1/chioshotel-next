249e20d43e8ee19f18136f49fe6c990c54190dd3
stratosv1 <evoulamandis@gmail.com>
2026-05-31 12:45:49 +0300

    Improve room detail schema images

--- content/room-detail-schema.ts
+++ content/room-detail-schema.ts
@@ -1,7 +1,70 @@
 import type { RoomDetailData } from "@/content/room-details";
 
+function uniqueValues(values: string[]) {
+  return Array.from(new Set(values.filter(Boolean)));
+}
+
+function getRoomDetailImages(data: RoomDetailData) {
+  const heroImage = data.hero.image;
+  const ogImage = data.seo.ogImage;
+
+  const galleryImages = data.gallery.images.map((image) => image.src);
+
+  const individualRoomImages = data.individualRooms.rooms.flatMap((room) =>
+    room.images.map((image) => image.src),
+  );
+
+  return uniqueValues([heroImage, ogImage, ...galleryImages, ...individualRoomImages]);
+}
+
+function getIndividualRoomSchemas(data: RoomDetailData, canonicalUrl: string) {
+  return data.individualRooms.rooms.map((room) => ({
+    "@type": "HotelRoom",
+    "@id": `${canonicalUrl}#${room.id}`,
+    name: room.name,
+    description: room.description,
+    url: canonicalUrl,
+    image: room.images.map((image) => image.src),
+    occupancy: {
+      "@type": "QuantitativeValue",
+      maxValue: room.maxGuests,
+    },
+    containedInPlace: {
+      "@id": "https://chioshotel.gr/#hotel",
+    },
+    amenityFeature: room.amenities.map((amenity) => ({
+      "@type": "LocationFeatureSpecification",
+      name: amenity.label,
+      value: true,
+    })),
+    additionalProperty: [
+      {
+        "@type": "PropertyValue",
+        name: "Room type",
+        value: room.type,
+      },
+      {
+        "@type": "PropertyValue",
+        name: "Location",
+        value: room.location,
+      },
+      {
+        "@type": "PropertyValue",
+        name: "Beds",
+        value: room.beds.join(", "),
+      },
+      {
+        "@type": "PropertyValue",
+        name: "Highlights",
+        value: room.badges.join(", "),
+      },
+    ],
+  }));
+}
+
 export function buildRoomDetailSchema(data: RoomDetailData) {
   const canonicalUrl = `https://chioshotel.gr${data.seo.canonicalPath}`;
+  const allImages = getRoomDetailImages(data);
 
   return {
     "@context": "https://schema.org",
@@ -12,7 +75,7 @@ export function buildRoomDetailSchema(data: RoomDetailData) {
         name: data.hero.title,
         description: data.seo.description,
         url: canonicalUrl,
-        image: data.gallery.images.map((image) => image.src),
+        image: allImages,
         containedInPlace: {
           "@id": "https://chioshotel.gr/#hotel",
         },
@@ -22,7 +85,11 @@ export function buildRoomDetailSchema(data: RoomDetailData) {
           value: true,
           description: item.text,
         })),
+        hasPart: data.individualRooms.rooms.map((room) => ({
+          "@id": `${canonicalUrl}#${room.id}`,
+        })),
       },
+      ...getIndividualRoomSchemas(data, canonicalUrl),
       {
         "@type": "BreadcrumbList",
         "@id": `${canonicalUrl}#breadcrumb`,
