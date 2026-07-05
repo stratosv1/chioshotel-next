ff3d9c27122ab02f344a62a5555699ecb01de29f
stratosv1 <44983464+stratosv1@users.noreply.github.com>
2026-07-05 21:11:50 +0300

    Load room wizard styles on English rooms page

--- app/chios-rooms/layout.tsx
+++ app/chios-rooms/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "../css-split/pages/rooms-category.css";
+import "../css-split/pages/room-wizard.css";
 
 export default function RouteCssLayout({
   children,
