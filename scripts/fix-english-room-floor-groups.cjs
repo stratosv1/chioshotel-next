817b38f07c6bdb88e45170ed19442a69af89347a
stratosv1 <evoulamandis@gmail.com>
2026-07-07 21:24:04 +0300

    Load floor group styles on English room pages

---
+++ scripts/fix-english-room-floor-groups.cjs
@@ -0,0 +1,20 @@
+﻿const fs = require("fs");
+
+const files = [
+  "app/chios-rooms/standard-double-room/layout.tsx",
+  "app/chios-rooms/economy-double-rooms/layout.tsx",
+];
+
+for (const file of files) {
+  let s = fs.readFileSync(file, "utf8");
+
+  if (!s.includes('room-detail-floor-groups.css')) {
+    s = s.replace(
+      'import "../../css-split/pages/room-detail-cards.css";',
+      'import "../../css-split/pages/room-detail-cards.css";\nimport "../../css-split/pages/room-detail-floor-groups.css";'
+    );
+  }
+
+  fs.writeFileSync(file, s, "utf8");
+  console.log(`Patched ${file}`);
+}
