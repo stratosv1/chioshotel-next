7d811376c240fe65bc310d13d3bac80cf22df258
stratosv1 <44983464+stratosv1@users.noreply.github.com>
2026-07-11 01:50:20 +0300

    Remove legacy mobile compact room CSS import

--- app/chios-rooms/family-chios-apartments/layout.tsx
+++ app/chios-rooms/family-chios-apartments/layout.tsx
@@ -2,7 +2,6 @@ import type { ReactNode } from "react";
 import "../../css-split/pages/room-detail.css";
 import "../../css-split/pages/room-detail-cards.css";
 import "../../css-split/pages/room-detail-i18n-fix.css";
-import "../../css-split/overrides/room-detail-mobile-compact.css";
 
 export default function RouteCssLayout({
   children,
