d87bd372502ba0b05e2e54067ab908fba3228e13
stratosv1 <44983464+stratosv1@users.noreply.github.com>
2026-07-11 01:57:45 +0300

    Remove room detail legacy CSS imports

--- app/chios-rooms/family-chios-apartments/layout.tsx
+++ app/chios-rooms/family-chios-apartments/layout.tsx
@@ -1,7 +1,4 @@
 import type { ReactNode } from "react";
-import "../../css-split/pages/room-detail.css";
-import "../../css-split/pages/room-detail-cards.css";
-import "../../css-split/pages/room-detail-i18n-fix.css";
 
 export default function RouteCssLayout({
   children,
