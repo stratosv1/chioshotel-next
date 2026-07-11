e344649dfae306df3c01063c109ebbef02960d55
stratosv1 <44983464+stratosv1@users.noreply.github.com>
2026-07-11 13:09:45 +0300

    Use refined Tailwind homepage for localized pages

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { HomePageTailwind } from "@/components/home/HomePageTailwind";
+import { HomePageTailwindV2 } from "@/components/home/HomePageTailwindV2";
 import { JsonLd } from "@/components/seo/JsonLd";
 import {
   homePageDe,
@@ -81,7 +81,7 @@ export default async function Page({ params }: PageProps) {
   return (
     <>
       <JsonLd data={buildHomePageSchema(data)} />
-      <HomePageTailwind data={data} />
+      <HomePageTailwindV2 data={data} />
     </>
   );
 }
