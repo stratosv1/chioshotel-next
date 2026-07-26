c8ff7b2248eeece7e38233f45dd15030f3938404
stratosv1 <44983464+stratosv1@users.noreply.github.com>
2026-07-26 12:08:16 +0300

    Add economy room carousel wrapper

---
+++ components/rooms/EconomyRoomDetailPage.tsx
@@ -0,0 +1,17 @@
+import { RoomDetailPage } from "@/components/rooms/RoomDetailPage";
+import type { RoomDetailData } from "@/content/room-details";
+
+type EconomyRoomDetailPageProps = {
+  data: RoomDetailData;
+};
+
+export function EconomyRoomDetailPage({ data }: EconomyRoomDetailPageProps) {
+  return (
+    <RoomDetailPage
+      data={{
+        ...data,
+        id: "economy-double-carousel",
+      }}
+    />
+  );
+}
