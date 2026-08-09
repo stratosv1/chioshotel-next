1c2949c458094b41a781985738f5da7d11ce7436
stratosv1 <44983464+stratosv1@users.noreply.github.com>
2026-08-09 12:29:19 +0300

    Add Open-Meteo marine forecast diagnostics

--- content/trip-planner/index.ts
+++ content/trip-planner/index.ts
@@ -2,5 +2,6 @@ export * from "./beaches";
 export * from "./beach-routing";
 export * from "./marine-exposure";
 export * from "./marine-comfort";
+export * from "./marine-forecast";
 export * from "./villages";
 export * from "./village-routing";
