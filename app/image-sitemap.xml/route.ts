f3386807c557a57d4491bef23940992067e4bfec
stratosv1 <44983464+stratosv1@users.noreply.github.com>
2026-08-21 13:40:20 +0300

    feat: build image sitemap from room galleries

--- app/image-sitemap.xml/route.ts
+++ app/image-sitemap.xml/route.ts
@@ -1,8 +1,69 @@
+import type { RoomDetailData } from "@/content/room-details";
+import {
+  economyDoubleRoomsDe,
+  economyDoubleRoomsEl,
+  economyDoubleRoomsEn,
+  economyDoubleRoomsEs,
+  economyDoubleRoomsFr,
+  economyDoubleRoomsIt,
+  economyDoubleRoomsTr,
+  familyChiosApartmentsDe,
+  familyChiosApartmentsEl,
+  familyChiosApartmentsEn,
+  familyChiosApartmentsEs,
+  familyChiosApartmentsFr,
+  familyChiosApartmentsIt,
+  familyChiosApartmentsTr,
+  standardDoubleRoomDe,
+  standardDoubleRoomEl,
+  standardDoubleRoomEn,
+  standardDoubleRoomEs,
+  standardDoubleRoomFr,
+  standardDoubleRoomIt,
+  standardDoubleRoomTr,
+} from "@/content/room-details";
+import {
+  economyDoubleRoomsPl,
+  familyChiosApartmentsPl,
+  standardDoubleRoomPl,
+} from "@/content/room-details-pl";
 import { absoluteUrl } from "@/lib/seo";
 import { getAllSeoImageSets } from "@/lib/seo-image-registry";
 import { getSeoImagesForPath } from "@/lib/seo-image-schema";
 
 export const dynamic = "force-static";
+
+type ImageSitemapEntry = {
+  path: string;
+  images: string[];
+};
+
+const roomDetailPages: readonly RoomDetailData[] = [
+  standardDoubleRoomEn,
+  standardDoubleRoomEl,
+  standardDoubleRoomFr,
+  standardDoubleRoomDe,
+  standardDoubleRoomIt,
+  standardDoubleRoomEs,
+  standardDoubleRoomTr,
+  standardDoubleRoomPl,
+  economyDoubleRoomsEn,
+  economyDoubleRoomsEl,
+  economyDoubleRoomsFr,
+  economyDoubleRoomsDe,
+  economyDoubleRoomsIt,
+  economyDoubleRoomsEs,
+  economyDoubleRoomsTr,
+  economyDoubleRoomsPl,
+  familyChiosApartmentsEn,
+  familyChiosApartmentsEl,
+  familyChiosApartmentsFr,
+  familyChiosApartmentsDe,
+  familyChiosApartmentsIt,
+  familyChiosApartmentsEs,
+  familyChiosApartmentsTr,
+  familyChiosApartmentsPl,
+];
 
 function escapeXml(value: string) {
   return value
@@ -13,29 +74,60 @@ function escapeXml(value: string) {
     .replaceAll("'", "&apos;");
 }
 
+function unique(values: readonly string[]) {
+  return Array.from(new Set(values));
+}
+
+function getRoomPageImages(page: RoomDetailData) {
+  return unique([
+    page.hero.image,
+    ...page.gallery.images.map((image) => image.src),
+    ...page.individualRooms.rooms.flatMap((room) =>
+      room.images.map((image) => image.src),
+    ),
+  ]);
+}
+
+function mergeEntries(entries: readonly ImageSitemapEntry[]) {
+  const byPath = new Map<string, Set<string>>();
+
+  for (const entry of entries) {
+    const images = byPath.get(entry.path) ?? new Set<string>();
+    entry.images.forEach((image) => images.add(image));
+    byPath.set(entry.path, images);
+  }
+
+  return Array.from(byPath, ([path, images]) => ({
+    path,
+    images: Array.from(images),
+  }));
+}
+
 export function GET() {
-  const urls = getAllSeoImageSets()
-    .map((set) => {
-      const images = getSeoImagesForPath(set.path)
+  const registryEntries: ImageSitemapEntry[] = getAllSeoImageSets().map((set) => ({
+    path: set.path,
+    images: getSeoImagesForPath(set.path).map((image) => image.src),
+  }));
+
+  const roomEntries: ImageSitemapEntry[] = roomDetailPages.map((page) => ({
+    path: page.seo.canonicalPath,
+    images: getRoomPageImages(page),
+  }));
+
+  const urls = mergeEntries([...registryEntries, ...roomEntries])
+    .filter((entry) => entry.images.length > 0)
+    .map((entry) => {
+      const images = entry.images
         .map(
-          (image) => `
-    <image:image>
-      <image:loc>${escapeXml(absoluteUrl(image.src))}</image:loc>
-    </image:image>`,
+          (image) => `\n    <image:image>\n      <image:loc>${escapeXml(absoluteUrl(image))}</image:loc>\n    </image:image>`,
         )
         .join("");
 
-      return `
-  <url>
-    <loc>${escapeXml(absoluteUrl(set.path))}</loc>${images}
-  </url>`;
+      return `\n  <url>\n    <loc>${escapeXml(absoluteUrl(entry.path))}</loc>${images}\n  </url>`;
     })
     .join("");
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">${urls}
-</urlset>`;
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">${urls}\n</urlset>`;
 
   return new Response(xml, {
     headers: {
